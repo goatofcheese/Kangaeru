cee64b2b052f132d17dfb59d74c5b1dbd335d6de
goatofcheese <jawells@clemson.edu>
2013-01-29 19:04:06 -0500

    bleh

--- src/edu/clemson/kangaeru/NotecardActivity.java
+++ src/edu/clemson/kangaeru/NotecardActivity.java
@@ -4,6 +4,9 @@ import java.util.ArrayList;
 import java.util.List;
 
 import android.app.Activity;
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
@@ -22,6 +25,7 @@ public class NotecardActivity extends Activity {
         setTitle("Notecards");
         setContentView(R.layout.activity_notecard);
         
+        //Spinner Stuff
         listselect = (Spinner) findViewById(R.id.listspinner);
         List<String> list = new ArrayList<String>();
         list.add("Fakelist 1");
@@ -38,7 +42,17 @@ public class NotecardActivity extends Activity {
         		openList(v);
         	}
         });
+        //End of Spinner stuff 
+       
+        //Notecard Fragment
+        FragmentManager fragmentManager = getFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        NotecardFragment notecardDisplay = new NotecardFragment();
+        //Fragment notecardDisplay = fragmentManager.findFragmentById(R.id.fragment_content);
+        fragmentTransaction.add(R.id.fragment, notecardDisplay);
+        fragmentTransaction.commit();
         
+        //Return button
         Button b = (Button) findViewById(R.id.returnbutton);
         b.setOnClickListener(new View.OnClickListener() {
 			public void onClick(View v) {
@@ -47,7 +61,6 @@ public class NotecardActivity extends Activity {
 				startActivity(i.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP));
 			}
 		});
-        
         
     }
 
